8678e0b26690683c026e7f3a808a30feb465b825
Facu <facundo.mendez@live.com.ar>
2022-04-06 19:00:35 -0300

    .

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
+import "./login.css"
 const Login = () => {
   return (
-    <div><h1>Pagina de login</h1></div>
+    <div className="boxFormulario">
+      <section className='formularioContacto'>
+        <div className="form">
+          <div className="input">
+            <div className="inputBox">
+              <label>Username</label>
+              <input type="email" placeholder="Example@gmail.com" required />
+            </div>
+            <div className="inputBox">
+              <label>Password</label>
+              <input type="password" placeholder="........"required />
+            </div>
+            <div className="inputBox">
+              <button id="button">
+                <Link className='singIn' to="/User">Sing In</Link>
+              </button>
+            </div>
+          </div>
+          <p>Forget password? <a href="#">Click Here</a></p>
+        </div>
+    </section>
+    </div>
+
   )
 }
 
