18fed1f45e691feb23a2915ae4a9416d71ed1301
Facu <facundo.mendez@live.com.ar>
2022-05-03 18:18:40 -0300

    .

--- src/components/container1/PresentGame.jsx
+++ src/components/container1/PresentGame.jsx
@@ -20,15 +20,21 @@ const PresentGame = () => {
                 <video src={videoAlienAttack}  preload="none" autoPlay loop muted  ></video>
             </div>
 
-            
+            <div class="text middle" data-text="Welcome to the world of AlienAttack!">Welcome to the world of AlienAttack!</div>
 
             <div className="textPresent">
                 <p>
-                Welcome to the world of AlienAttack, a multiplayer strategy game where you have to acquire only one character and fight against other people.
+                A multiplayer strategy game where you have to acquire only one character and fight against other people.
                 Inside the map you can find hidden tokens, you will be able to earn tokens by eliminating your opponents and level up your character.
                 With the tokens you can buy Stats and thus increase the potential of your champion! Compete against your friends in custom tournaments and earn customization emblems!
                 </p>
             </div>
+
+            <svg class="arrows">
+							<path class="a1" d="M0 0 L30 32 L60 0"></path>
+							<path class="a2" d="M0 20 L30 52 L60 20"></path>
+							<path class="a3" d="M0 40 L30 72 L60 40"></path>
+						</svg>
         </div>
     )
     
--- src/components/container1/presentGameEffects.js
+++ src/components/container1/presentGameEffects.js
@@ -4,6 +4,20 @@ import ScrollTrigger from 'gsap/ScrollTrigger'
 const presentGameEffects = () => {
     
     gsap.registerPlugin(ScrollTrigger); 
+
+
+    gsap.from(".middle",{
+        opacity:-2,
+        scale:2.5,
+        duration:2.5,
+        delay:.5,
+        scrollTrigger:{
+            trigger: ".container1",
+            scrub: 2,
+        }
+    })
+
+
     gsap.from(".textPresent",{
         opacity:-2,
         scale:2.5,
